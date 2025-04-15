e06eb61cf9b826b65cc32e906e52336145271f6b
Yun Chen <azppl2013@gmail.com>
2025-04-15 21:35:17 +0800

    add:divide

---
+++ NTNU-GDG/git-tutorial-2/assignments/team1/Calculator/operator.py
@@ -0,0 +1,20 @@
+def add(x, y):
+    return x + y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero")
+    return x / y
+
+# ============================================
+# 請在下方區塊新增你們自訂的運算子函數定義
+# 例如小組 A 可新增其它運算子：
+#   '-': subtract
+#   '*': multiply
+#   '/': divide
+# ============================================
+
+operators = {
+    '+': add
+    '/':divide
+}
